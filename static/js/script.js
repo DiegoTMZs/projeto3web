a90fe1d9e90cff9364f7cbe189b9547003dbbfe0
Diego Tomaz Estrela <163549182+DiegoTMZs@users.noreply.github.com>
2025-08-27 11:07:25 -0300

    adição de contador de caractere, e validador de formulário

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,34 @@ btn.onclick = function() {
 span.onclick = function() {
     modal.style.display = "none";
 }
+
+
+if (document.getElementById('recipeForm')) {
+
+    const modoPreparoInput = document.getElementById('modo-preparo');
+    const charCountSpan = document.getElementById('charCount');
+
+    modoPreparoInput.addEventListener('input', function() {
+        const currentLenght = modoPreparoInput.value.length;
+        charCountSpan.textContent = currentLenght;
+    });
+
+    const form = document.getElementById('recipeForm');
+    const nomeReceitaInput = document.getElementById('nome-receita');
+    const errorMenssage = document.getElementById('errorMensage');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        if (nomeReceitaInput.value.trim() === '') {
+            errorMenssage.textContent = 'Por favor, preencha o nome da receita.';
+        
+        } else {
+            errorMenssage.textContent = ''
+
+            alert('Receita adicionada com sucesso!');
+            form.reset();
+            charCountSpan.textContent = 0;
+        }
+    });
+}
