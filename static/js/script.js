0b8d9b2c4e8d7bb734f0584a0fffc45b6fd20817
Diego Tomaz Estrela <163549182+DiegoTMZs@users.noreply.github.com>
2025-08-27 10:12:46 -0300

    correção da importação do javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,5 @@
 const modal = document.getElementById("loginModal");
-
 const btn = document.getElementById("loginBtn");
-
 const span = document.getElementById("close-btn")[0];
 
 btn.onclick = function() {
